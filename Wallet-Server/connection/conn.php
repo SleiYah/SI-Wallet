66e7b8cbdd3419a41fa5a11236eaee0480f4ee1b
SleiYah <shy300602@gmail.com>
2025-03-06 03:34:00 +0200

    i hope this works

--- Wallet-Server/connection/conn.php
+++ Wallet-Server/connection/conn.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Methods: GET, OPTIONS, POST');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
    
-    $conn = new mysqli("localhost", "root", "S13iy@h02", "si_wallet_db");
+    $conn = new mysqli("localhost", "root", 'S13iy@h02', "si_wallet_db");
 
 
 if ($conn->connect_error) {

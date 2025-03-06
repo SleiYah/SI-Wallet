314e37c92a708623c4bc45c8be33b73d042e61ce
SleiYah <shy300602@gmail.com>
2025-03-06 02:39:47 +0200

    i hope this works

--- Wallet-Server/connection/conn.php
+++ Wallet-Server/connection/conn.php
@@ -1,8 +1,8 @@
 <?php 
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET, OPTIONS, POST');
-// header('Access-Control-Allow-Headers: Content-Type, Authorization');
-// header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); 
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Origin, Accept'); 
+header('Content-Type: application/json'); 
 
    
     $conn = new mysqli("localhost", "root", '$l3iyah', "si_wallet_db");

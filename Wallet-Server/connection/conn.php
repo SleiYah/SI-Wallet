056c40440d18b2f9976ff718ed4b678fb101423a
SleiYah <shy300602@gmail.com>
2025-03-06 03:00:49 +0200

    i hope this works

--- Wallet-Server/connection/conn.php
+++ Wallet-Server/connection/conn.php
@@ -1,5 +1,5 @@
 <?php 
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, OPTIONS, POST');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
